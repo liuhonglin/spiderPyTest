f9ba696bbcfedf7141135e3028d8d4134e087673
liuhonglin <liuhonglinh@163.com>
2017-07-23 21:01:35 +0800

    save data to mongo

--- ZhiLian/zhilian_kw_spider.py
+++ ZhiLian/zhilian_kw_spider.py
@@ -15,7 +15,7 @@ from urllib.parse import urlencode
 from multiprocessing import Pool
 import requests
 from bs4 import BeautifulSoup
-#import pymongo
+import pymongo
 #import zhilian_kw_config
 import time
 from itertools import product
@@ -25,6 +25,14 @@ TOTAL_PAGE_NUMBER = 1; #PAGE_NUMBER: total number of pages，可进行修改
 KEYWORDS = ["spark", "python", "java", "阿里", "腾讯", "京东", "爱奇艺"] #需爬取的关键字可以自己添加或修改
 
 ADDRESS = ["北京", "上海", "深圳"]
+
+MONGO_URI = "127.0.0.1"
+MONGO_PORT = "27017"
+MONGO_DB = "spider"
+
+client = pymongo.MongoClient(MONGO_URI, int(MONGO_PORT))
+db = client[MONGO_DB]
+db.authenticate("spider", "spider@lhl")
 
 # 设置代理服务器
 proxies= {
@@ -66,6 +74,16 @@ def get_content(html):
             brief = tr_brief.find('li', {'class':'newlist_deatil_last'}).get_text()
 
             # 用生成器获取信息
+            """
+                yield 的作用就是把一个函数变成一个 generator，带有 yield 的函数不再是一个普通函数，Python 解释器会将其视为一个 generator，
+                调用 get_content(html) 不会执行 get_content 函数，而是返回一个 iterable 对象！
+                在 for 循环执行时，每次循环都会执行 get_content 函数内部的代码，执行到 yield b 时，fab 函数就返回一个迭代值，
+                下次迭代时，代码从 yield b 的下一条语句继续执行，而函数的本地变量看起来和上次中断执行前是完全一样的，于是函数继续执行，直到再次遇到 yield。
+                
+                当函数执行结束时，generator 自动抛出 StopIteration 异常，表示迭代完成。在 for 循环里，无需处理 StopIteration 异常，循环会正常结束。
+                
+                yield 的好处是显而易见的，把一个函数改写为一个 generator 就获得了迭代能力，比起用类的实例保存状态来计算下一个 next() 的值，不仅代码简洁，而且执行流程异常清晰。
+            """
             yield {'zwmc': zwmc,
                 'fkl': fkl,
                 'gsmc': gsmc,
@@ -80,6 +98,8 @@ def get_content(html):
 def main(args):
     basic_url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?'
 
+    table = db["zhilian"]
+
     for keyword in KEYWORDS:
 
         paras = {'jl':args[0],
@@ -93,12 +113,13 @@ def main(args):
         if html:
             data = get_content(html)
             for item in data:
-                print('保存记录：', item)
+                if table.update({"zw_link":item['zw_link']}, {'$set':item}, True):
+                    print('已保存记录：', item)
 
 if __name__ == '__main__':
     start = time.time()
     number_list = list(range(TOTAL_PAGE_NUMBER))
-    args = product(ADDRESS, number_list)
+    args = product(ADDRESS, number_list) # 笛卡儿集
     pool = Pool()
     pool.map(main, args) # 多进程运行
     end = time.time()
